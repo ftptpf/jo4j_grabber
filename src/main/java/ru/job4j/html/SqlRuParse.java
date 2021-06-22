29d1a17e860a8abb92887001c0728bbd4a838af9
ftptpf <inboxru@yandex.ru>
2021-06-22 20:29:25 +0300

    2. Парсинг HTML страницы. [#260358]

---
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -0,0 +1,26 @@
+package ru.job4j.html;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+
+/**
+ * Получение информации о вакансиях с сайта https://www.sql.ru/forum/job-offers
+ */
+public class SqlRuParse {
+    public static void main(String[] args) throws IOException {
+        Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers").get();
+        Elements row = doc.select(".postslisttopic");
+        Elements dataRows = doc.select(".altCol");
+        for (Element td : row) {
+            Element href = td.child(0);
+            System.out.println(href.attr("href"));
+            System.out.println(href.text());
+            Element data = td.parent().child(5);
+            System.out.println(data.text());
+        }
+    }
+}
