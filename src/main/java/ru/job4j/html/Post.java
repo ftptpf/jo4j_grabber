7a54af63e5ddf8c50cbc81268102752d03aea454
ftptpf <Nb49Rtdd>
2021-06-29 10:43:29 +0300

    2.2. Модель данных - Post. [#285211]

---
+++ src/main/java/ru/job4j/html/Post.java
@@ -0,0 +1,79 @@
+package ru.job4j.html;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+/**
+ * Модель данных Post.
+ */
+public class Post {
+    private int id; // идентификатор вакансии (берется из нашей базы данных)
+    private String title; // название вакансии
+    private String link; // ссылка на описание вакансии
+    private String description; // описание вакансии
+    private LocalDateTime created; // дата создания вакансии
+
+    public Post(String title, String link, String description, LocalDateTime created) {
+        this.title = title;
+        this.link = link;
+        this.description = description;
+        this.created = created;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDateTime getCreated() {
+        return created;
+    }
+
+    public void setCreated(LocalDateTime created) {
+        this.created = created;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Post post = (Post) o;
+        return id == post.id && title.equals(post.title) && link.equals(post.link) && description.equals(post.description) && created.equals(post.created);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, title, link, description, created);
+    }
+}
