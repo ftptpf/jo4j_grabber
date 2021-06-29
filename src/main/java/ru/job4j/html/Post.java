21268cca46b6f8114096b758ca160fb0cceb75d9
ftptpf <Nb49Rtdd>
2021-06-29 16:54:29 +0300

    2.2. Модель данных - Post. [#285211] - without description

--- src/main/java/ru/job4j/html/Post.java
+++ src/main/java/ru/job4j/html/Post.java
@@ -69,11 +69,11 @@ public class Post {
             return false;
         }
         Post post = (Post) o;
-        return id == post.id && title.equals(post.title) && link.equals(post.link) && description.equals(post.description) && created.equals(post.created);
+        return id == post.id && title.equals(post.title) && link.equals(post.link) && created.equals(post.created);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, title, link, description, created);
+        return Objects.hash(id, title, link, created);
     }
 }
