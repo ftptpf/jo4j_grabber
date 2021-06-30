d7e2d7ab81ca1b236abf47d4719c2f8521fb468c
ftptpf <Nb49Rtdd>
2021-06-30 15:25:22 +0300

    2.3. Загрузка деталей поста. [#285212]

--- src/main/java/ru/job4j/html/SqlRuParse.java
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -17,15 +17,28 @@ public class SqlRuParse {
             String url = "https://www.sql.ru/forum/job-offers/" + i;
             Document doc = Jsoup.connect(url).get();
             Elements row = doc.select(".postslisttopic");
-            Elements dataRows = doc.select(".altCol");
             for (Element td : row) {
                 Element href = td.child(0);
                 System.out.println(href.attr("href"));
                 System.out.println(href.text());
+                System.out.println(loadPost(href.attr("href")));
                 Element data = td.parent().child(5);
                 System.out.println(data.text());
                 System.out.println(new SqlRuDateTimeParser().parse(data.text()));
             }
         }
+    }
+
+    /**
+     * Получаем подробное описание вакансии.
+     * @param url ссылка на вакансию
+     * @return текст вакансии
+     * @throws IOException
+     */
+    private static String loadPost(String url) throws IOException {
+        Document docPost = Jsoup.connect(url).get();
+        Element table = docPost.select("table[class=msgTable]").first();
+        Element post = table.select("td[class=msgBody]").get(1);
+        return post.text();
     }
 }
